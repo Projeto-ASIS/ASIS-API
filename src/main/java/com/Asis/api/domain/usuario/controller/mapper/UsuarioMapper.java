76376fecf717962af837f49f4e9276e089d5b528
Alisson Gabriel Pereira de Lima <147565148+AlissonLimaG@users.noreply.github.com>
2025-03-23 13:42:05 -0300

    feat: autenticação básica implentada

---
+++ src/main/java/com/Asis/api/domain/usuario/controller/mapper/UsuarioMapper.java
@@ -0,0 +1,12 @@
+package com.Asis.api.domain.usuario.controller.mapper;
+import com.Asis.api.domain.usuario.controller.DTOs.UsuarioCadastroRequestDTO;
+import com.Asis.api.domain.usuario.entity.UsuarioEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface UsuarioMapper {
+    UsuarioMapper INSTANCE = Mappers.getMapper(UsuarioMapper.class);
+
+    UsuarioEntity dtoToEntity(UsuarioCadastroRequestDTO usuarioCadastroRequestDTO);
+}
--- src/main/java/com/Asis/api/infra/security/SecurityConfig.java
+++ src/main/java/com/Asis/api/infra/security/SecurityConfig.java
@@ -2,11 +2,15 @@ package com.Asis.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,13 +21,24 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .sessionManagement(session -> session
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(autorize -> autorize
                         .requestMatchers("/h2-console/**").permitAll()
-                        .requestMatchers("/swagger-ui.html/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
                         .anyRequest().permitAll())
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .build();
     }
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 }
