760f7e331a43809aa9c54d454e4bd4322c377c1e
Alisson Gabriel Pereira de Lima <147565148+AlissonLimaG@users.noreply.github.com>
2025-03-23 09:56:07 -0300

    chore(gitignore): adicionado gitignore e removendo diretórios de configuração da ide e diretório de build 'target'.

--- target/classes/db/migration/V1__create_table_servico.sql
+++
@@ -1,9 +0,0 @@
--- Criando tabela de serviços
-
-CREATE TABLE tb_servico(
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    descricao VARCHAR(500) NOT NULL,
-    comoAcessar VARCHAR(500) NOT NULL,
-    linkAcesso VARCHAR(100) NULL
-);
